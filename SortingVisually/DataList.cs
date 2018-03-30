4f3c9dad49b251095299595aa3dbf7f79ae78e52
Bazanova Nadezhda <bazanovanv.gmail.com>
2018-03-30 19:37:30 +0300

    Working variant

---
+++ SortingVisually/DataList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.ComponentModel;
+
+namespace SortingVisually
+{
+
+
+    class DataList : INotifyPropertyChanged
+    {
+        private int number;
+        public int Number
+        {
+            get {return number; }
+            set
+            {
+                if (value == number) return;
+                number = value;
+                OnPropertyChanged(nameof(Number));
+            }
+        }
+
+        private Brush myColor;
+        public Brush MyColor
+        {
+            get { return myColor; }
+            set
+            {
+                if (value == myColor) return;
+                myColor = value;
+                OnPropertyChanged(nameof(MyColor));
+            }
+        }
+
+        public DataList(int number, Brush myColor)
+        {
+            Number = number;
+            MyColor = myColor;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        internal void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- SortingVisually/MainWindow.xaml.cs
+++ SortingVisually/MainWindow.xaml.cs
@@ -1,116 +1,84 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
+using System.Drawing;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading.Tasks;
 
 namespace SortingVisually
 {
+    
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        int[] quantity;
-        const int N = 10;
+        ObservableCollection<DataList> dList;
+        const int N = 5;
 
         public MainWindow()
         {
             InitializeComponent();
-            
+            dList = new ObservableCollection<DataList>();
+            lbox.ItemsSource = dList;
         }
 
         private void Swap(int a, int b)
         {
-            int tmp = quantity[a];
-            quantity[a] = quantity[b];
-            quantity[b] = tmp;
+            var tmp = dList[a];
+            dList[a] = dList[b];
+            dList[b] = tmp;
+
+        }
+
+        async private void BubbleSort()
+        {
+            for (int i = 0; i < N - 1; i++)
+            {
+                for (int j = 1; j < N; j++)
+                {
+                    dList[j].MyColor = Brushes.Green;
+                    dList[j - 1].MyColor = Brushes.Green;
+                    await Task.Delay(400);
+                    if (dList[j].Number < dList[j - 1].Number)
+                    {
+                        await Task.Delay(400);
+
+                        Swap(j, j - 1);
+                        await Task.Delay(400);
+
+                    }
+                    await Task.Delay(400);
+
+                    dList[j].MyColor = Brushes.Red;
+                    dList[j - 1].MyColor = Brushes.Red;
+                }
+            }
         }
 
         private void Start_Click(object sender, RoutedEventArgs e)
         {
-            //int j;
-            
-
-            for (int i = 0; i < N-1; i++)
-            {
-                
-                for (int j = 1; j < N-i; j++)
-                {
-                    {
-                        Binding binding = new Binding();
-                        //binding.ElementName = "j";
-                        //binding.Path = new PropertyPath("j");
-                        binding.Source = j;
-                        //lblText.SetBinding(Label.ContentProperty, binding);
-                        BindingOperations.SetBinding(lblText, Label.ContentProperty, binding);
-                        lblText.Content = j + "<" + (j - 1);
-
-                    }
-                    
-                    Thread.Sleep(100);
-                    if (quantity[j] < quantity[j - 1])
-                    {
-                        Swap(j, j - 1);
-                    }
-                }
-            }
-            MessageBox.Show("The end");
-           
+            BubbleSort();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             Random rnd = new Random();
-            quantity = new int[N];
+            
             for (int i = 0; i < N; i++)
             {
-                quantity[i] = rnd.Next(1, 200);
+                dList.Add(new DataList(rnd.Next(1,200), Brushes.Red));
             }
 
-            Grid grid = new Grid();
-            grid.RowDefinitions.Add(new RowDefinition());
-            grid.VerticalAlignment = VerticalAlignment.Bottom;
-            Basic_sp.Children.Add(grid);
-
-
-            for (int i = 0; i < N; i++)
-            {
-                StackPanel spnl = new StackPanel();
-                spnl.VerticalAlignment = VerticalAlignment.Bottom;
-
-                TextBlock lbl_T = new TextBlock();
-                lbl_T.Text = String.Format("{0}", quantity[i]);
-                lbl_T.Foreground = new SolidColorBrush(Colors.Red);
-                lbl_T.HorizontalAlignment = HorizontalAlignment.Center;
-                spnl.Children.Add(lbl_T);
-
-                Label lbl_L = new Label();
-                lbl_L.Background = new SolidColorBrush(Colors.Black);
-                lbl_L.Height = quantity[i];
-                lbl_L.HorizontalAlignment = HorizontalAlignment.Center;
-                spnl.Children.Add(lbl_L);
-
-                TextBlock lbl_B = new TextBlock();
-                lbl_B.Text = String.Format("{0}", i);
-                lbl_B.Foreground = new SolidColorBrush(Colors.Green);
-                lbl_B.HorizontalAlignment = HorizontalAlignment.Center;
-                spnl.Children.Add(lbl_B);
-
-                grid.ColumnDefinitions.Add(new ColumnDefinition());
-                Grid.SetRow(spnl, 0);
-                Grid.SetColumn(spnl, i);
-                grid.Children.Add(spnl);
-            }
         }
     }
 }
